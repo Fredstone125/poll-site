3347d5522a04a431e826d319775a3a7213fa7609
Fredstone125 <jakekeeven9@gmail.com>
2026-02-19 12:18:48 -0600

    Create getLeaderboard.js

---
+++ api/getLeaderboard.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+  const response = await fetch(
+    `${supabaseUrl}/rest/v1/reaction_scores?order=time.asc`,
+    {
+      headers: {
+        apikey: supabaseKey,
+        Authorization: `Bearer ${supabaseKey}`
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
